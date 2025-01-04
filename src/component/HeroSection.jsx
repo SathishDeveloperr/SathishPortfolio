6b3f85ebac3c92041a484998a31e137019b3319f
Sathish Pooja <Sathish Pooja@DESKTOP-S39ETOQ>
2025-01-04 20:14:23 +0530

    Test Mobile

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -1,27 +1,31 @@
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
+import profile from "../assets/profile-pictures/DarkImg2.png";
 
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-      Sathish Kumar
-          
-      </h1>
-      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-      Hi, I’m Sathish, a seasoned Software Engineer and full-stack developer with 2.5 years in designing, developing, and delivering scalable software solutions. My expertise spans a wide range of technologies, including .NET, MERN, Angular, Django, FastAPI, and Flutter. I have successfully contributed to multiple projects, from enterprise-grade web applications to cross-platform mobile solutions, showcasing my ability to adapt and excel in diverse environments.
-      </p>
-      <div className="flex justify-center my-10">
-        <a
-          href="#"
-          className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
-        >
-          Start for free
-        </a>
-        <a href="#" className="py-3 px-4 mx-3 rounded-md border">
-          Documentation
-        </a>
+
+
+      <div className="flex flex-col lg:flex-row justify-center items-center my-10 px-4">
+        <div className="flex-1 flex justify-center items-center mt-6 lg:mt-0">
+          <img
+            src={profile}
+            className="w-60 h-60 sm:w-72 sm:h-72 lg:w-80 lg:h-80 rounded-r-full"
+            alt="Profile"
+          />
+        </div>
+        <div className="flex-1 lg:pr-6 py-3">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl text-center lg:text-left tracking-wide">
+            Sathish Kumar
+          </h1>
+          <p className="mt-6 text-base sm:text-lg lg:text-xl text-center lg:text-left text-neutral-500 max-w-4xl lg:max-w-none">
+            Hi, I’m Sathish, a seasoned Software Engineer and full-stack developer with 2.5 years in designing, developing, and delivering scalable software solutions. My expertise spans a wide range of technologies, including .NET, MERN, Angular, Django, FastAPI, and Flutter. I have successfully contributed to multiple projects, from enterprise-grade web applications to cross-platform mobile solutions, showcasing my ability to adapt and excel in diverse environments.
+          </p>
+        </div>
       </div>
+
+
       <div className="flex mt-10 justify-center">
         <video
           autoPlay
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,6 +1,6 @@
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/Icon2.png";
 import { navItems } from "../constants";
 
 const navbar = () => {
@@ -15,8 +15,7 @@ const navbar = () => {
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
-            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
-            <span className="text-xl tracking-tight">VirtualR</span>
+            <img className="h-10 w-50 mr-2" src={logo} style={{filter:'invert(0)'}} alt="Logo" />
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
